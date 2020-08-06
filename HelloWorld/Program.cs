3a073d51c120220380e73cd93d240ade25859597
김강용 <bizk2y@gmail.com>
2020-08-06 10:55:03 +0900

    .

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 콘솔 출력
+            Console.WriteLine("Hello World!");
+            // 아무키나 눌러주세요.
+            Console.WriteLine("Press Any Key ...");
+            Console.ReadKey();
+        }
+    }
+}
